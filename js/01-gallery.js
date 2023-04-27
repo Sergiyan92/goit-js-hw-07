9912fabf65c5db3b02bafc55f450ec7804d7bd38
Sergiyan92 <sergiyan@ukr.net>
2023-04-27 19:30:29 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const createElements = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+gallery.innerHTML = createElements;
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  const targetImage = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+     <img src="${targetImage}" width="800" height="600">
+`);
+  instance.show();
+});
 console.log(galleryItems);
